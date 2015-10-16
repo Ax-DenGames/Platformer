dd276f495150d2724c4a4f042ba96e2d6e6029a7
Ax-DenGames <axdengames@gmail.com>
2015-10-16 18:10:27 +0300

    +Score
    I add score system to project

---
+++ Assets/Scripts/ScoreEvents/ScoreManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ScoreManager : MonoBehaviour {
+    public UnityEngine.UI.Text txt;
+    public int score;
+
+    void UpdateScore()
+    {
+        txt.text = "Score: " + score;
+    }
+
+    internal void AddPoint(int adPoint)
+    {
+        score = score + adPoint;
+        UpdateScore();
+    }
+}
---
+++ Assets/Scripts/ScoreEvents/TriggerScore.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerScore : MonoBehaviour {
+
+    public ScoreManager _mng;
+    public int AdPoint;
+    // Use this for initialization
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            _mng.AddPoint(AdPoint);
+            Destroy(this.gameObject);
+        }
+    }
+}
